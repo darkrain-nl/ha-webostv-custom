0e6e14a702fa8baccc0a33bf70e1b6af07829a2d
darkrain-nl <24763370+darkrain-nl@users.noreply.github.com>
2026-07-19 11:30:56 +0200

    Update webostv custom component with reviewer feedback (v1.0.2)

--- custom_components/webostv/switch.py
+++ custom_components/webostv/switch.py
@@ -1,11 +1,10 @@
-"""Support for LG webOS TV switches."""
+"""Support for LG webOS TV switch."""
 
 from collections.abc import Callable, Coroutine
 from functools import wraps
-import logging
-from typing import Any, Concatenate, cast, override
+from typing import Any, Concatenate, override
 
-from aiowebostv import WebOsClient, WebOsTvState
+from aiowebostv import WebOsClient, WebOsTvCommandError, WebOsTvState
 
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.core import HomeAssistant
@@ -13,10 +12,10 @@ from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
-from .const import DOMAIN, WEBOSTV_EXCEPTIONS
-from .helpers import WebOsTvConfigEntry
+from . import WebOsTvConfigEntry
+from .const import DOMAIN
 
-_LOGGER = logging.getLogger(__name__)
+PARALLEL_UPDATES = 0
 
 
 async def async_setup_entry(
@@ -24,60 +23,67 @@ async def async_setup_entry(
     entry: WebOsTvConfigEntry,
     async_add_entities: AddConfigEntryEntitiesCallback,
 ) -> None:
-    """Set up the LG webOS TV switches."""
-    async_add_entities([LgWebOSScreenSwitchEntity(entry)])
+    """Set up the LG webOS TV switch platform."""
+    client = entry.runtime_data
+
+    async_add_entities([LgWebOSScreenSwitchEntity(entry, client)])
 
 
 def cmd[_R, **_P](
-    func: Callable[
-        Concatenate["LgWebOSScreenSwitchEntity", _P], Coroutine[Any, Any, _R]
-    ],
-) -> Callable[Concatenate["LgWebOSScreenSwitchEntity", _P], Coroutine[Any, Any, _R]]:
+    func: Callable[Concatenate[LgWebOSScreenSwitchEntity, _P], Coroutine[Any, Any, _R]],
+) -> Callable[Concatenate[LgWebOSScreenSwitchEntity, _P], Coroutine[Any, Any, _R]]:
     """Catch command exceptions."""
 
     @wraps(func)
     async def cmd_wrapper(
-        self: "LgWebOSScreenSwitchEntity", *args: _P.args, **kwargs: _P.kwargs
+        self: LgWebOSScreenSwitchEntity, *args: _P.args, **kwargs: _P.kwargs
     ) -> _R:
         """Wrap all command methods."""
         try:
             return await func(self, *args, **kwargs)
-        except WEBOSTV_EXCEPTIONS as error:
+        except WebOsTvCommandError as exc:
             raise HomeAssistantError(
                 translation_domain=DOMAIN,
                 translation_key="communication_error",
                 translation_placeholders={
-                    "name": str(self._entry.title),
                     "func": func.__name__,
-                    "error": str(error),
+                    "name": self._entry.title,
+                    "error": str(exc),
                 },
-            ) from error
+            ) from exc
 
     return cmd_wrapper
 
 
 class LgWebOSScreenSwitchEntity(SwitchEntity):
-    """Representation of an LG webOS TV screen switch."""
+    """Representation of a LG webOS TV Screen Switch."""
 
     _attr_has_entity_name = True
     _attr_translation_key = "screen"
-    _attr_name = "Screen"
 
-    def __init__(self, entry: WebOsTvConfigEntry) -> None:
+    def __init__(self, entry: WebOsTvConfigEntry, client: WebOsClient) -> None:
         """Initialize the screen switch entity."""
         self._entry = entry
-        self._client: WebOsClient = entry.runtime_data
+        self._client = client
         self._attr_unique_id = f"{entry.unique_id}_screen"
-        self._update_states()
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, entry.unique_id)},
+            name=entry.title,
+        )
 
     @override
     async def async_added_to_hass(self) -> None:
-        """Connect and subscribe to state updates."""
+        """Connect to client signals."""
         await super().async_added_to_hass()
         await self._client.register_state_update_callback(
             self.async_handle_state_update
         )
         self._update_states()
+
+    @override
+    async def async_will_remove_from_hass(self) -> None:
+        """Unregister state update callback."""
+        self._client.unregister_state_update_callback(self.async_handle_state_update)
 
     async def async_handle_state_update(self, tv_state: WebOsTvState) -> None:
         """Update state from WebOsClient."""
@@ -85,22 +91,18 @@ class LgWebOSScreenSwitchEntity(SwitchEntity):
         self.async_write_ha_state()
 
     def _update_states(self) -> None:
-        """Update entity states."""
-        tv_state = self._client.tv_state
-        self._attr_is_on = tv_state.is_screen_on
-        self._attr_available = tv_state.is_on
-        self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, cast(str, self._entry.unique_id))},
-            manufacturer="LG",
-            name=self._entry.title,
-        )
+        """Update entity attributes."""
+        self._attr_available = self._client.tv_state.is_on
+        self._attr_is_on = self._client.tv_state.is_screen_on
 
     @cmd
+    @override
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn the screen on."""
         await self._client.request("com.webos.service.tvpower/power/turnOnScreen")
 
     @cmd
+    @override
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn the screen off."""
         await self._client.request("com.webos.service.tvpower/power/turnOffScreen")
